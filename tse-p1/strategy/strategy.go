a0f3a07aae08eb77e9faa3122172cfe48a31c050
themaslowsdilemma <themaslowsdilemma@gmail.com>
2025-10-01 00:06:31 -0400

    fix merge issues

--- tse-p1/strategy/strategy.go
+++ tse-p1/strategy/strategy.go
@@ -6,10 +6,6 @@ import (
 )
 
 type Strategy interface {
-<<<<<<< Updated upstream
-	Decide(candles []candles.Candle, currentIndex int) (market.Action, float64)
-=======
 	Decide(candles []candles.Candle, i int) (market.Action, float64)
->>>>>>> Stashed changes
 	GetName() string
 }
