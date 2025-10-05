485528cb47af7d3adc8caf5ac3ddd5c511bd01be
themaslowsdilemma <themaslowsdilemma@gmail.com>
2025-10-05 16:52:14 -0400

    ledger GetItemString

--- tse-p2/ledger/ledger.go
+++ tse-p2/ledger/ledger.go
@@ -1,8 +1,21 @@
 package ledger
 
+import (
+    "fmt"
+)
+
 type LedgerItem interface {
-    Hash() []byte
-    Copy() LedgerItem
+    Hash()      []byte
+    Copy()      LedgerItem
+    String()    string
 }
 
 type Ledger map[uint64]LedgerItem
+
+func (l *Ledger) GetItemString(id uint64) (string, err) {
+    var li LedgerItem = l[id]
+    if li == nil {
+        return "", fmt.Errorf("ledger contains no value for id: %v", id)
+    }
+    return li.String(), nil
+}
