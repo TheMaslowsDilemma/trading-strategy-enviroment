fbe8dee1705073294187e07d1010115310f22282
themaslowsdilemma <themaslowsdilemma@gmail.com>
2025-10-08 17:58:10 -0400

    simulation miner

--- tse-p2/simulation/simulation-miner.go
+++ tse-p2/simulation/simulation-miner.go
@@ -1,3 +1,28 @@
 package simulation
 
-func (sim *Simulation) 
+// Standard Pattern for Simulation Entities:
+// 1. infinite for loop
+// 2. select to either end task & cleanup or iterate entity
+// 3. Optional Delay
+func (sim *Simulation) minerTask() {
+	for {
+		select {
+			case <-sim.CancelChan:
+				cleanMinerTask()
+				return
+			default:
+				iterateMinerTask()
+				break
+		}
+		// thread.Sleep
+	}
+}
+
+
+func (sim *Simulation) iterateMinerTask() {
+
+}
+
+func (sim *Simulation) cleanMinerTask() {
+
+} 
