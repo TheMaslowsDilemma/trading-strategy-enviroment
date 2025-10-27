f3c63756c033d143169f589a5dd94e964ed27b28
themaslowsdilemma <themaslowsdilemma@gmail.com>
2025-10-27 08:35:13 -0400

    use uint256

--- tse-p3/go.mod
+++ tse-p3/go.mod
@@ -1,3 +1,5 @@
 module tse-p3
 
 go 1.25.2
+
+require github.com/holiman/uint256 v1.3.2
--- tse-p3/main.go
+++ tse-p3/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/holiman/uint256"
 )
 
 func main() {
 	fmt.Println("--- Trading Stategy Environment: Part Three ---")
+	
+	var (
+		c1    *uint256.Int
+		c2    *uint256.Int
+		big1  *uint256.Int
+		big2  *uint256.Int
+		bigres *uint256.Int
+	)
+
+	c1    = uint256.NewInt(7829)
+	c2    = uint256.NewInt(6703)
+	big1  = uint256.NewInt(123456)
+	big2  = uint256.NewInt(987654)
+	bigres = uint256.NewInt(0)
+	for i := 0; i < 40; i++ {
+		bigres.Add(big1, big2)
+		fmt.Printf("%02d: %v + %v = %v\n", i, big1, big2, bigres)
+		big1.Add(big1, c1)
+		big2.Or(big2.Add(big2, c2), bigres)
+	}
+
 }
